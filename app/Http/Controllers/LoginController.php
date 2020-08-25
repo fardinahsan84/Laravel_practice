eb43cc87c4caa9f862314480f09f789a7ac8418a
fardinahsan84@gmail.com <fardinahsan84@gmail.com>
2020-08-25 23:19:03 +0600

    basic setup

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+
+    function index(){
+
+    	return view('login.index');
+    }
+
+    function validation(Request $request){
+
+    	if($request->username == $request->password){
+    		//session
+    		return redirect('/home');
+    	}
+      else{
+        return redirect('/login');
+      }
+    }
+}
